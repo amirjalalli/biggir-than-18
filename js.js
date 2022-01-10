625d103e2864ccc2d8b573714fb26d5ce8042b8c
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2022-01-10 21:56:46 +0330

    practice-javascript0001

---
+++ js.js
@@ -0,0 +1,9 @@
+let num1 = +(prompt("enter first number: "))
+let num2 = +(prompt("enter second number: "))
+
+let plusResult = num1 + num2
+let minesResult = num1 - num2 
+let zarbResult = num1 * num2
+let tavanResult = num1 ** num2 
+
+console.log(plusResult,minesResult,zarbResult,tavanResult)
